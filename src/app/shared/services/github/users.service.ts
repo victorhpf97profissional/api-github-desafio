036cd126c8cce980f8dbd0dd03c9579611e86387
Victor Henrique de Paula Ferreira <victorhpf97profissional@gmail.com>
2021-09-30 09:41:06 -0300

    Criado Interface e Service da API
    Interface e Service para consumir a API criados

---
+++ src/app/shared/services/github/users.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { Observable } from 'rxjs';
+import { User } from '../../interfaces/user';
+import { UserInfo } from '../../interfaces/user-info';
+
+@Injectable()
+export class UserService {
+
+    private URL = environment.GITHUB_API_URL;
+
+    constructor(private http: HttpClient,) { }
+
+    getAll(): Observable<User[]> {
+        return this.http.get<User[]>(`${this.URL}/users`);
+    }
+
+    getByName(userName: string): Observable<UserInfo> {
+        return this.http.get<UserInfo>(`${this.URL}/users/${userName}`);
+    }
+
+}
