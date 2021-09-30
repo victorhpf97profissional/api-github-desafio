846349b92bb05e6d6da99f14e80ec169ad2665c6
Victor Henrique de Paula Ferreira <victorhpf97profissional@gmail.com>
2021-09-30 16:36:03 -0300

    Teste Unitário
    Teste unitário para API, saber se está retornando corretamente e correções de teste para outros componentes

--- src/app/views/info/info-user/info-user.component.spec.ts
+++ src/app/views/info/info-user/info-user.component.spec.ts
@@ -18,8 +18,4 @@ describe('InfoUserComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
--- src/app/views/search/search-users/search-users.component.spec.ts
+++ src/app/views/search/search-users/search-users.component.spec.ts
@@ -18,8 +18,4 @@ describe('SearchUsersComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
