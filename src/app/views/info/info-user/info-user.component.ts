38c35a5ab703ff2d98df6f114bd400de950b7ae8
Victor Henrique de Paula Ferreira <victorhpf97profissional@gmail.com>
2021-09-30 14:48:30 -0300

    Ordenando repositório por número de estrelas
    Função para ordenar repositório criada

--- src/app/views/info/info-user/info-user.component.ts
+++ src/app/views/info/info-user/info-user.component.ts
@@ -8,7 +8,7 @@ import { UsersService } from 'src/app/shared/services/github/users.service';
   styleUrls: ['./info-user.component.scss']
 })
 export class InfoUserComponent implements OnInit {
-  public userInfo: any = { user: null, repository: null, followers: null };
+  public userInfo: any = { user: [], repository: [], followers: [] };
 
   constructor(
     private route: ActivatedRoute,
@@ -42,8 +42,7 @@ export class InfoUserComponent implements OnInit {
 
   loadUserRepository(userName: string) {
     this.serviceUser.getRepository(userName).subscribe(repository => {
-      this.userInfo.repository = repository;
-      console.warn({ repository: repository })
+      this.userInfo.repository = this.orderRepositoryByStars(repository);;
     });
   }
 
@@ -51,6 +50,10 @@ export class InfoUserComponent implements OnInit {
     this.serviceUser.getFollowers(userName).subscribe(followers => {
       this.userInfo.followers = followers;
     });
+  }
+
+  orderRepositoryByStars(repository: any) {
+    return repository.sort((a: any, b: any) => { return b.stargazers_count - a.stargazers_count });
   }
 
 
