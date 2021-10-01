12b866e38ced90d19788173216ea9346c49d6e09
Victor Henrique de Paula Ferreira <victorhpf97profissional@gmail.com>
2021-09-30 22:10:46 -0300

    Firebase Deploy
    Hospedado no firebase

--- src/app/views/info/info-user-dialog/info-user-dialog.component.spec.ts
+++ src/app/views/info/info-user-dialog/info-user-dialog.component.spec.ts
@@ -18,8 +18,4 @@ describe('InfoUserDialogComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: false,
+  GITHUB_API_URL: 'https://api.github.com',
+  GITHUB_API_KEY: '',
 };
